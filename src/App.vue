c282ac00f2c7b3062fbdb15eafa31a600263ee6c
YooBin Jeong <jyb1018@gmail.com>
2021-12-22 00:36:10 +0900

    using states, event-handler, v-for, binding, modal, import/export

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,56 @@
 <template>
 
+  <div class="black-bg" v-if="isModalOpen">
+    <div class="white-bg">
+      <h4>{{modaltitle}}</h4>
+      <p>{{modalcontent}}</p>
+      <button @click="isModalOpen = false" class="closemodal-btn">닫기</button>
+    </div>
+  </div> 
+
+
   <div class="menu">
-    
     <a v-for="(item, idx) in 메뉴들 " :key="idx">{{idx}}) {{item}}</a>
   </div>
 
 
-  <!-- <div v-for="(item, idx) in products" :key="idx" class="section">
-    <h4>{{item}}</h4>
-    <p>{{prices[idx]}}</p>
+
+  <div v-for="(item, idx) in data" :key="idx">
+    <img :src="item.image" class="room-img">
+    <h4 @click="openmodal(idx)">{{item.title}}</h4>
+    <p>{{item.price}}원</p>
   </div>
-   -->
+  
 </template>
 
 <script>
+
+import dataarr from './assets/data.js';
 
 export default {
   name: 'App',
   data() {
     return {
+      // UI State 공간, State라고도 부름
+      // 현재 UI(ex. 모달, 탭, 등등) 상태
+      data : dataarr,
+      isModalOpen : false,
       메뉴들 : ['Home', 'Products', 'About'],
       products: ['천호동원룸', '강호동원룸', '아몰랑'],
-      prices: ['50만원', '60만원', '70만원'],
+      신고수 : [0, 0, 0],
+
+      modaltitle : "기본 모달 타이틀",
+      modalcontent : "기본 모달 내용",
+    }
+  },
+  methods: {
+    increase(idx) {
+      this.신고수[idx]++;
+    },
+    openmodal(idx) {
+      this.isModalOpen = true;
+      this.modaltitle = this.data[idx].title;
+      this.modalcontent = this.data[idx].content;
     }
   },
   components: {
@@ -30,6 +59,11 @@ export default {
 </script>
 
 <style>
+
+.room-img {
+  width: 50%;
+  margin-top: 40px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -48,5 +82,33 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position:fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.closemodal-btn {
+  position: relative;
+  top: 1px;
+  left: 1px;
 }
 </style>
