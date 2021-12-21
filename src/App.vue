925fd9e5e9271741ca7447be41fa4848f2307aee
YooBin Jeong <jyb1018@gmail.com>
2021-12-21 22:46:59 +0900

    v-for 써보기

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="menu">
+    
+    <a v-for="(item, idx) in 메뉴들 " :key="idx">{{idx}}) {{item}}</a>
+  </div>
+
+
+  <!-- <div v-for="(item, idx) in products" :key="idx" class="section">
+    <h4>{{item}}</h4>
+    <p>{{prices[idx]}}</p>
+  </div>
+   -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      메뉴들 : ['Home', 'Products', 'About'],
+      products: ['천호동원룸', '강호동원룸', '아몰랑'],
+      prices: ['50만원', '60만원', '70만원'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +36,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
